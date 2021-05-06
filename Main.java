c68e6c734813d68242e7c2946ad0fb3e1e626bd9
SimTec-hub <80486892+SimTec-hub@users.noreply.github.com>
2021-05-06 15:21:43 +0000

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,70 +1,42 @@
-package geekbrain;
+package sample;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("ЧАТ");
+        primaryStage.setScene(new Scene(root, 600, 600));
+        primaryStage.show();
+
+
+
+    }
+    public void start2(Stage primaryStage) throws Exception{
+        //FXMLLoader loader = new FXMLLoader(StartGraphicsHere.class.getResource("sample2.fxml"));
+        //AnchorPane page = (AnchorPane) loader.load();
+        //Stage dialogStage = new Stage();
+        //dialogStage.setTitle("Edit Profile");
+        //dialogStage.initModality(Modality.WINDOW_MODAL);
+        //dialogStage.initOwner(primaryStage);
+        //Scene scene = new Scene(page);
+        //dialogStage.setScene(scene);
+    }
 
     public static void main(String[] args) {
-	String[] words = {
-        "Тверь",
-        "Ржев",
-        "Торопец",
-        "Торжок",
-        "Кимры",
-        "Конаково",
-         "Удомля",
-         "Бежецк",
-         "Ржев",
-         "Осташков",
-         "Ржев",
-         "Калязин",
-         "Торопец",
-         "Удомля",
-         "Озёрный",
-         "Кувшиново",
-         "Кимры",
-         "Старица",
-         "Максатиха",
-         "Калязин"};
-        ArrayList<String> unique_words = new ArrayList<>();
-        HashMap<String, Integer> counter = new HashMap<>();
-        int x = 0;
-	for (int i = 0; i < words.length; i++) {
-	    if (!unique_words.contains(words[i])){
-	        unique_words.add(words[i]);
-	        counter.put(words[i], 1);
-	    }
-	    else
-        {
-            x = counter.get(words[i]);
-            x = x +1;
-            counter.replace(words[i], x);
-        }
-    }
-    Iterator iterator = unique_words.iterator();
-	while (iterator.hasNext()) {
-	    System.out.println(iterator.next());
-        }
-	//System.out.println(counter);
-        for (HashMap.Entry<String, Integer> pair : counter.entrySet())
-        {
-            String key = pair.getKey();
-            int value = pair.getValue();
-            System.out.println("Количество слов " + key + " = " + value);
-        }
-
-        Phone_direct book = new Phone_direct();
-        book.add("Петров", "8-555-777-23");
-        book.add("Маслов", "8-555-798-56");
-        book.add("Петров", "8-555-777-23");
-        book.add("Жуков", "8-555-688-20");
-        book.add("Петров", "8-555-000-23");
-
-        System.out.println("Номер телефона Петрова: " + book.get("Петров"));
-        System.out.println("Номер телефона Жукова: " + book.get("Жуков"));
+        launch(args);
     }
 
+    public class StartGraphicsHere {
 
+    }
 }
